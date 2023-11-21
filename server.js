f16e73c5863bb3f905959e78a53b2cd63f24da98
eakulp <eymixita@gmail.com>
2023-11-20 23:41:26 -0800

    Week10 tasks updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();//app is a backend application
 
 app.get("/",(req,res)=>{
-    res.send("Welcome to my Sean's backend application");
+    res.send("Welcome to my Eymi's backend application");
 });// forward slash means the context root of the application, or the url without anything after it
 
 app.get("/sean",(req,res)=>{
